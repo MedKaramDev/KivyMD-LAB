4f7694c8378c003ec39d2f9880950106af499e2b
MedKaramDev <ahmed.ak.karam@gmail.com>
2021-10-19 17:00:35 +0100

    Exemple Theming

--- FirstApp/main.py
+++ FirstApp/main.py
@@ -1,7 +1,11 @@
 from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+from kivy.lang import Builder
 
 class DemoApp(MDApp):
     def build(self):
-        return 
-
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palette = 'Teal'
+        self.theme_cls.accent_palette = 'Blue'
+        return Builder.load_file('color_theme.kv')
 DemoApp().run()
