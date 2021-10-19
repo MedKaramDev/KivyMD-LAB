1a7af7516562d397b78cdd35f5f34bb11fb1dc26
MedKaramDev <ahmed.ak.karam@gmail.com>
2021-10-19 17:41:26 +0100

    Login Screen

---
+++ LoginScreen/main.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+
+class MainApp(MDApp):
+    def build(self):
+        self.theme_cls.theme_style = 'Dark'
+        self.theme_cls.primary_palette = 'BlueGray'
+        return Builder.load_file('login.kv')
+
+MainApp().run()
